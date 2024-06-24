f64712f68bb22a7f43a256034f36ccfa51127f37
anatasija4572 <74258980+anatasija4572@users.noreply.github.com>
2024-06-24 21:16:37 +0200

    commit

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	
+	$(".largeGrid").click(function(){											
+    $(this).find('a').addClass('active');
+    $('.smallGrid a').removeClass('active');
+    
+    $('.product').addClass('large').each(function(){											
+		});						
+		setTimeout(function(){
+			$('.info-large').show();	
+		}, 200);
+		setTimeout(function(){
+
+			$('.view_gallery').trigger("click");	
+		}, 400);								
+		
+		return false;				
+	});
+	
+	$(".smallGrid").click(function(){		        
+    $(this).find('a').addClass('active');
+    $('.largeGrid a').removeClass('active');
+    
+		$('div.product').removeClass('large');
+		$(".make3D").removeClass('animate');	
+    $('.info-large').fadeOut("fast");
+		setTimeout(function(){								
+				$('div.flip-back').trigger("click");
+		}, 400);
+		return false;
+	});		
+	
+	$(".smallGrid").click(function(){
+		$('.product').removeClass('large');			
+		return false;
+	});
+  
+});
